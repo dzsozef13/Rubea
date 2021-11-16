78fd5cfa19144ed72e9e66e04f7ac5424fece333
József Ádám Punk <punkjoad@gmail.com>
2021-11-16 02:10:34 +0100

    ready to submit eyy

--- rubea-webshop/src/App.vue
+++ rubea-webshop/src/App.vue
@@ -12,8 +12,12 @@
 </template>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -34,7 +38,6 @@ body {
 #burger-button {
   display: none;
 }
-
 
 @media screen and (max-width:600px) {
   #navbar-container {
